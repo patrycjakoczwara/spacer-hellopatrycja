b87a177bff7198ea8fe3a7f3a11ce928034a6f01
Patrycja Koczwara <patrycjakoczwara@gmail.com>
2019-02-10 19:03:09 +0100

    wip

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Search from './views/Search.vue';
 
 
 Vue.use(Router);
@@ -11,7 +11,7 @@ export default new Router({
   routes: [{
       path: '/',
       name: 'home',
-      component: Home,
+      component: Search,
     },
     {
       path: '/about',
